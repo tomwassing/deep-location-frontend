205a10373ab689be25421009fe8b94d2303b322a
Tom J. Wassing <tjwassing@gmail.com>
2022-10-25 14:11:04 +0200

    Added comments

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,6 @@
 <template>
 
-<div class="w-100 h-100">
-
+  <div class="w-100 h-100">
     <div class="fixed-top mx-2 my-2" style="width: 300px">
       <div class="card" style="width: 300px" v-if="current">
         <img :src="current.url" class="card-img-top">
@@ -9,25 +8,19 @@
           <h5 class="card-title">{{ current.id }}</h5>
           <h6 class="card-subtitle mb-2 text-muted">{{ parseDate(current) }}</h6>
           <p class="card-text">
-            <h6 class="my-1">Accuracy: <span class="fw-bolder">{{ (current.accuracy * 100).toFixed(2) }}&#x25;</span></h6>
-            <div class="progress">
-              <div class="progress-bar" :style="{ width: current.accuracy * 100 + '%' }" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
-            </div>
-            <h6 class="my-1">Tags</h6>
-            <span class="badge bg-primary me-1" v-for="tag in parseTags(current)">{{ tag }}</span>
+          <h6 class="my-1">Accuracy: <span class="fw-bolder">{{ (current.accuracy * 100).toFixed(2) }}&#x25;</span></h6>
+          <div class="progress">
+            <div class="progress-bar" :style="{ width: current.accuracy * 100 + '%' }" aria-valuenow="100"
+              aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+          <h6 class="my-1">Tags</h6>
+          <span class="badge bg-primary me-1" v-for="tag in parseTags(current)">{{ tag }}</span>
           </p>
-          <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
         </div>
       </div>
-
-      <!-- <div class="card mt-2" style="width: 300px">
-        <canvas class="card-img-top" id="myChart" width="300" height="300" ></canvas>
-      </div> -->
     </div>
-
     <div id="mapContainer" class="w-100 vh-100 d-inline-block"></div>
-
-    </div>
+  </div>
 </template>
 
 <script>
@@ -35,7 +28,6 @@ import mapboxgl from "mapbox-gl";
 import data from "../data/temp.json";
 import S2 from "s2-geometry";
 import * as turf from '@turf/turf';
-import Chart from 'chart.js/auto';
 
 export default {
   name: "BaseMap",
@@ -50,7 +42,7 @@ export default {
       if (entry.tags) {
         return entry.tags.split(",")
           .map((tag) => decodeURIComponent(tag.trim())
-          .replaceAll("+", " "));
+            .replaceAll("+", " "));
       }
 
       return [];
@@ -69,35 +61,7 @@ export default {
   mounted() {
     mapboxgl.accessToken = this.accessToken;
 
-    // const myChart = new Chart("myChart", {
-    //     type: 'scatter',
-    //     data: {
-    //       datasets: [{
-    //         label: 'Scatter Dataset',
-    //         data: [{x: -10,y: 0}, {
-    //           x: 0,
-    //           y: 10
-    //         }, {
-    //           x: 10,
-    //           y: 5
-    //         }, {
-    //           x: 0.5,
-    //           y: 5.5
-    //         }],
-    //         backgroundColor: 'rgb(255, 99, 132)'
-    //       }],
-    //     },
-    //     options: {
-    //         scales: {
-    //             y: {
-    //                 beginAtZero: true
-    //             }
-    //         },
-    //         responsive: false,
-    //         showLine: true
-    //     }
-    // });
-
+    // Initialize map, zoomed on the VU.
     const map = new mapboxgl.Map({
       container: "mapContainer",
       style: "mapbox://styles/mapbox/streets-v11",
@@ -106,8 +70,9 @@ export default {
     });
 
 
-      // extend mapboxGL Marker so we can pass in an onClick handler
-        class ClickableMarker extends mapboxgl.Marker {
+    // extend mapboxGL Marker so we can pass in an onClick handler, based on:
+    // https://bl.ocks.org/chriswhong/8977c0d4e869e9eaf06b4e9fda80f3ab
+    class ClickableMarker extends mapboxgl.Marker {
       // new method onClick, sets _handleClick to a function you pass in
       onClick(handleClick) {
         this._handleClick = handleClick;
@@ -126,72 +91,75 @@ export default {
       }
     };
 
+    // Template of GeoJSON data for distance line.
     const geojson = {
-        'type': 'FeatureCollection',
-        'features': []
+      'type': 'FeatureCollection',
+      'features': []
     };
 
+    // Template of LineString to show the distance line.
     const linestring = {
-        'type': 'Feature',
-        "properties": {},
-        'geometry': {
+      'type': 'Feature',
+      "properties": {},
+      'geometry': {
         'type': 'LineString',
         'coordinates': []
-        }
+      }
     };
 
     map.on('load', () => {
 
+      // Add a GeoJSON source containing place coordinates and information.
       map.addSource('geojson', {
         'type': 'geojson',
         'data': geojson
-        });
-        
+      });
 
-        map.addLayer({
-          id: 'distanceline',
-          type: 'line',
-          source: 'geojson',
-          layout: {
-            'line-cap': 'round',
-            'line-join': 'round',
-          },
-          paint: {
-            'line-color': 'gray',
-            'line-width': 1.5,
-            'line-dasharray': [6, 3],
-          },
-          filter: ['in', '$type', 'LineString']
-        });
+      // Add a layer to style the distance line.
+      map.addLayer({
+        id: 'distanceline',
+        type: 'line',
+        source: 'geojson',
+        layout: {
+          'line-cap': 'round',
+          'line-join': 'round',
+        },
+        paint: {
+          'line-color': 'gray',
+          'line-width': 1.5,
+          'line-dasharray': [6, 3],
+        },
+        filter: ['in', '$type', 'LineString']
+      });
 
-          map.addLayer({
-              "id": "symbols",
-              "type": "symbol",
-              "source": "geojson",
-              "layout": {
-                  "symbol-placement": "line-center",
-                  "text-field": '{title}',
-                  "text-size": 24
-              },
-              "paint": {}
-          });
+      // Add a layer to style the distance text.
+      map.addLayer({
+        "id": "symbols",
+        "type": "symbol",
+        "source": "geojson",
+        "layout": {
+          "symbol-placement": "line-center",
+          "text-field": '{title}',
+          "text-size": 24
+        },
+        "paint": {}
+      });
 
       var predictedMarker = null;
 
       for (const entry of data) {
-        console.log(entry);
         const marker = new ClickableMarker()
           .setLngLat([entry.lon, entry.lat])
-          .onClick(() => { // onClick() is a thing now!
-            this.current = entry;
-            // const popup = new mapboxgl.Popup()
-            // .setLngLat([entry.lon, entry.lat])
-            // .setHTML('<h1>' + entry.lon + '</h1>')
-            // .addTo(map);
+          .onClick(() => {
 
+            // Set the current entry.
+            this.current = entry;
+
+            // Convert predicted location to lat/lon.
             const latlng = S2.S2.idToLatLng(parseInt(entry.S2CellId, 16).toString());
             const predictedLocation = [latlng.lng, latlng.lat]
 
+            // Reset the previous predicted marker and distance line.
             if (predictedMarker) {
               predictedMarker.remove();
               linestring.geometry.coordinates = [];
@@ -199,42 +167,35 @@ export default {
               predictedMarker = null;
             }
 
-            predictedMarker = new mapboxgl.Marker({color: 'red'})
-            .setLngLat(predictedLocation)
-            .addTo(map);
+            // Add a new predicted marker
+            predictedMarker = new mapboxgl.Marker({ color: 'red' })
+              .setLngLat(predictedLocation)
+              .addTo(map);
 
-            map.fitBounds([[entry.lon, entry.lat],[latlng.lng, latlng.lat]], {
+            // Zoom to show both markers.
+            map.fitBounds([[entry.lon, entry.lat], [latlng.lng, latlng.lat]], {
               padding: 100
             });
 
+            // Set the distance line start and end.
             linestring.geometry.coordinates = [[entry.lon, entry.lat], predictedLocation]
 
+            // Add distance to line.
             const distance = turf.distance([entry.lon, entry.lat], predictedLocation);
-
             linestring.properties.title = distance.toLocaleString() + ' km';
 
+            // Add the line to the map.
             geojson.features.push(linestring);
-
             map.getSource('geojson').setData(geojson);
 
-            // popup.on('close', () => {
-            //   linestring.geometry.coordinates = [];
-            //   map.getSource('geojson').setData(geojson);
-
-            //   predictedMarker.remove();
-            // });
-        }).addTo(map);
+          }).addTo(map);
       }
-      });
+    });
   },
 };
 </script>
 
 
 <style scoped>
-
-
-
-
 
 </style>
